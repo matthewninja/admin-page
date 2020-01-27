1ec7dd7e1ae67060d864f61415a8ac82c95b3a79
matthewninja <matthewham4@gmail.com>
2020-01-27 13:37:58 -0500

    Removed superfluous code

--- src/index.js
+++ src/index.js
@@ -3,81 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import data from './data/users.json';
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    }
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
-  }
-
-  renderSquare(i) {
-    return <Square 
-      value={this.state.squares[i]}
-      onClick={() => {this.handleClick(i)} }
-    />;
-  }
-
-  render () {
-    const status = 'Next Player: X';
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => { this.props.onClick() } }>
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <div>{/* TODO */}</div>
-        </div>
-      </div>
-    );
-  }
-}
-
-// User object includes: 
-// name: string, companyName: string,  signUpDate: Date, 
-// lastVisitDate: Date, email: string, and phone: string. 
+/*
+ * User object includes: 
+ * name: string, companyName: string,  signUpDate: Date, 
+ * lastVisitDate: Date, email: string, and phone: string. 
+ */
 class UserTableItem extends React.Component {
   constructor(props) {
     super(props);
@@ -90,7 +20,6 @@ class UserTableItem extends React.Component {
   }
 
   render() {
-    // User Item in List
     return (
       <tr>
         <td>{this.name}</td>
@@ -104,7 +33,7 @@ class UserTableItem extends React.Component {
   }
 }
 
-class UserList extends React.Component {
+class UserTable extends React.Component {
   constructor(props) {
     super(props);
     this.users = props.users;
@@ -137,6 +66,6 @@ class UserList extends React.Component {
 // ================================
 
 ReactDOM.render(
-  <UserList users={data.users} />,
+  <UserTable users={data.users} />,
   document.getElementById('root')
 );
